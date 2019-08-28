0531c292ded44c9194a467368011b8200ed58921
Dong-JinKim <djnusa@kaist.ac.kr>
2019-08-28 15:18:53 +0900

    labelidx->gt_feat

--- densecap/modules/UnionSlicer.lua
+++ densecap/modules/UnionSlicer.lua
@@ -4,6 +4,9 @@ function layer:__init()
   parent.__init(self)
   self.grad_features = torch.Tensor()
   self.grad_gt_features = torch.Tensor()
+  
+  self.subj_index = nn.Index(1)
+  self.obj_index = nn.Index(1)
 end
 
 function layer:updateOutput(input)
@@ -13,7 +16,10 @@ function layer:updateOutput(input)
   if label_idx:nElement() == 0 then
     self.output = features--.new(1):zero()
   else
-    self.output = { features:index(1,label_idx[{1,{}}]) , features:index(1,label_idx[{2,{}}])  }
+    local out_subj = self.subj_index:forward{features,label_idx[{1,{}}]}
+    local out_obj  = self.obj_index:forward{features,label_idx[{2,{}}]}
+    
+    self.output = { out_subj , out_obj  }
   end
 
   return self.output
@@ -26,7 +32,8 @@ function layer:updateGradInput(input, gradOutput)
   if gt_features:nElement() == 0 then
     self.gradInput = {gradOutput, self.grad_gt_features}
   else
-    self.grad_features:resizeAs(features):zero()
+    --self.grad_features:resizeAs(features):zero()
+    self.grad_features = self.subj_index:backward({features,gt_features[{1,{}}]},gradOutput[1])[1] + self.obj_index:backward({features,gt_features[{2,{}}]},gradOutput[2])[1]
     self.gradInput = {self.grad_features, self.grad_gt_features}
   end
 
